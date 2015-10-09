438b4229af4b633ebc36e066e7017b90b26581e8
John Anderson <John@Johns-MacBook-Pro-2.local>
2015-10-08 19:16:56 -0500

    Renaming classes to make more sense. Changed the LinkedBlockingQueues to be of type order and pumpkins

--- src/Pumpkin.java
+++ src/Pumpkin.java
@@ -7,13 +7,24 @@ public class Pumpkin extends Thread {
     private double timeGrowing;
 
     public Pumpkin() {
-        this.timeGrowing = 0;
+        this.timeGrowing = 0; //Set growing time to 0
+    }
+
+    public long getGrowTime() {
+        RandomService randomService = new RandomService(1,2);
+        randomService.getTime(); //Enter in the parameters for the pumpkin grow time
+
+        return 0;
     }
 
 
     @Override
     public void run() {
-
+        try {
+            Thread.sleep(getGrowTime()); //sleep for amount of growing time
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- src/RandomService.java
+++ src/RandomService.java
@@ -1,5 +1,27 @@
 /**
  * Created by John on 10/8/15.
+ * This service will handle creating the random numbers for the program
+ * Each object that needs a random number will create an instance of this class to call into to get random number
+ * Must implement the guacian something random spread
+ * Will need to adjust the class variables depending on what we need to figure that out
  */
 public class RandomService {
+
+    private int variable1;
+    private  int variable2;
+
+    /**
+     * Constructor for the random class
+     *
+     * @param i int
+     * @param j int
+     */
+    public RandomService(int i, int j) {
+        this.variable1 = i;
+        this.variable2 = j;
+    }
+
+    public long getTime() {
+        return 1;
+    }
 }
